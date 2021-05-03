55d00186e12330393b8abaa52b1e8ac8bbd203ca
giacomodibiagio <dibiagio.giacomo@gmail.com>
2021-05-03 18:01:09 +0200

    dto finiti

--- src/main/java/it/prova/pizzastore/model/Ingrediente.java
+++ src/main/java/it/prova/pizzastore/model/Ingrediente.java
@@ -23,4 +23,53 @@ public class Ingrediente {
     private Set<Pizza> pizze = new HashSet<>(0);
 
 
+    public Ingrediente() {
+    }
+
+    public Ingrediente(Long id, String descrizione, Double prezzo, String codice) {
+        this.id=id;
+        this.descrizione=descrizione;
+        this.prezzo=prezzo;
+        this.codice=codice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public Set<Pizza> getPizze() {
+        return pizze;
+    }
+
+    public void setPizze(Set<Pizza> pizze) {
+        this.pizze = pizze;
+    }
 }
--- src/main/java/it/prova/pizzastore/model/Pizza.java
+++ src/main/java/it/prova/pizzastore/model/Pizza.java
@@ -29,10 +29,66 @@ public class Pizza {
 	private Set<Ordine> ordini = new HashSet<>(0);
 
 
+	public Pizza(Long id, String descrizione, String codice, Integer prezzoBase) {
+		this.id =id;
+		this.descrizione=descrizione;
+		this.codice=codice;
+		this.prezzoBase=prezzoBase;
+	}
 
-	
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-	
+	public String getDescrizione() {
+		return descrizione;
+	}
 
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public Integer getPrezzoBase() {
+		return prezzoBase;
+	}
+
+	public void setPrezzoBase(Integer prezzoBase) {
+		this.prezzoBase = prezzoBase;
+	}
+
+	public Integer getAttivo() {
+		return attivo;
+	}
+
+	public void setAttivo(Integer attivo) {
+		this.attivo = attivo;
+	}
+
+	public Set<Ingrediente> getIngredienti() {
+		return ingredienti;
+	}
+
+	public void setIngredienti(Set<Ingrediente> ingredienti) {
+		this.ingredienti = ingredienti;
+	}
+
+	public Set<Ordine> getOrdini() {
+		return ordini;
+	}
+
+	public void setOrdini(Set<Ordine> ordini) {
+		this.ordini = ordini;
+	}
 }
